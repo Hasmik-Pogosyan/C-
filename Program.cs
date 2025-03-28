ec176ebb4defa16d84a537a2349ad42a0a15f4d7
Hasmik-Pogosyan <hasmikpoghossian@outlook.com>
2025-03-28 14:31:23 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+
+namespace Dice_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerRandomNum, enemyRandomNum;
+            int playerPoint = 0,enemyPoint = 0;
+            Random rand = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Press any key to roll the dice.");
+                Console.ReadKey();
+
+                playerRandomNum = rand.Next(1, 7);
+                Console.WriteLine("You rolled a " + playerRandomNum);
+
+                Console.WriteLine("...");
+                System.Threading.Thread.Sleep(1000);
+
+                enemyRandomNum = rand.Next(1, 7);
+                Console.WriteLine("Enemy AI rolled a " + enemyRandomNum);
+
+                if (playerRandomNum > enemyRandomNum)
+                {
+                    playerPoint++;
+                }
+                else if (playerRandomNum < enemyRandomNum)
+                {
+                    enemyPoint++;
+                }
+                else
+                {
+                    Console.WriteLine("Draw");
+                }
+
+                Console.WriteLine("The score is now - Player " + playerPoint + " Enemy: " + enemyPoint);
+                Console.WriteLine();
+            }
+
+            if (playerPoint > enemyPoint)
+            {
+                Console.WriteLine("You win!");
+            }
+            else if (playerPoint < enemyPoint)
+            {
+                Console.WriteLine("You lose(");
+            }
+            else
+            {
+                Console.WriteLine("It's a draw!");
+            }
+        }
+    }
+}
